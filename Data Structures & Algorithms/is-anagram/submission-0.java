a7d1faa1ea8dc9606f5607435f412afebe8305b5
Lamiya Yasmin A S <119685766+LAMIYA16@users.noreply.github.com>
2026-09-07 21:12:19 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+
+        char[] sarray=s.toCharArray();
+        char[] tarray=t.toCharArray();
+
+        Arrays.sort(sarray);
+        Arrays.sort(tarray);
+
+        if(Arrays.equals(sarray,tarray))
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+}
